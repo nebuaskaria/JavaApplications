71ceba6177045f92ec5ba08db46d8aa8930dc88a
nebuaskaria <77119532+nebuaskaria@users.noreply.github.com>
2021-01-17 13:07:06 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,1 +1,10 @@
-def buildAppWithScript(){	echo "Building the application with Groovy Script"}return this
+def buildAppWithScript(){
+	echo "Building the application with Groovy Script"
+}
+
+//Groovy Script can access the environmental variables of Jenkins
+def testAppWithScript(){
+  echo "Testing the Application version : ${VERSION}"
+}
+
+return this
